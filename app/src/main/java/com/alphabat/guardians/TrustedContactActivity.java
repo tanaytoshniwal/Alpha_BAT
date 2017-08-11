5e9feac7917a0d521a4f66b14cdf2be5dee2e5ae
Tanay Toshniwal <tanaytoshniwal98@gmail.com>
2017-08-12 01:54:29 +0530

    First Commit

---
+++ app/src/main/java/com/alphabat/guardians/TrustedContactActivity.java
@@ -0,0 +1,13 @@
+package com.alphabat.guardians;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TrustedContactActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trusted_contact);
+    }
+}
